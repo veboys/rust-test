f33848e49784e64ce9f43b19d266c9e9f6d6ca73
root <root@localhost.localdomain>
2016-06-13 20:57:29 +0800

    update

--- test1/src/main.rs
+++ test1/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let (a, mut b):(bool,bool) = (true, false);
+    println!("a={:?}, b={:?}!", a, b);
+
+    let mut array:[i32;3] = [0;3];
+    array[1] = 1;
+    array[2] = 2;
+
+    for i in &array {
+        println!("{}", i);
+    }
 }
